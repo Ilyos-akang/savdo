bffce7f660270ba01fdff45ea2cbf09839b42edd
Ilyos <223318977+Ilyos-akang@users.noreply.github.com>
2026-02-26 00:15:03 +0500

    fix: alowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-83mvge6t*hj@1g#xk8g(h37di@w5n&c$lb+*!j5r57=ijmus10
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
